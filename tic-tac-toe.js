3087d83ceb8419db0423dc70aa674413395f1528
slie07 <liestevano@gmail.com>
2017-12-08 14:05:44 -0700

    setting up the winner function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,7 +5,7 @@ function startGame() {
 }
 
 function setMessage(msg) {
-	document.getElementById("message").innerText = msg;
+	/* setting the meassage */ document.getElementById("message").innerText = msg;
 }
 
 function nextMove(square) {
@@ -24,8 +24,41 @@ function switchTurn() {
 	} else {
 		document.turn = "X";
 	}
+	/* to take the turn mesaage */
 	setMessage(" It's " + document.turn + " turn !!");
 }
+
+ /* setting the winner */
+
+ function checkForWinner(move) {
+ 	var result = false;
+ 	if (checkRow(1, 2, 3, move) ||
+ 		checkRow(4, 5, 6, move) ||
+ 		checkRow(7, 8, 9, move) ||
+ 		checkRow(1, 4, 7, move) ||
+ 		checkRow(2, 5, 8, move) ||
+ 		checkRow(3, 6, 9, move) ||
+ 		checkRow(1, 5, 9, move) ||
+ 		checkRow(3, 5, 7, move)) {
+ 		result = true;
+ 	}
+ 	return result;
+ }
+
+function checkRow (a, b, c, move) {
+	var result = false;
+	if(getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+		result = true;
+	}
+	return result;
+}
+
+function getBox (number) {
+	return document.getElementById("s" + number).innerText;
+
+}
+
+
 
 
 	// body...
